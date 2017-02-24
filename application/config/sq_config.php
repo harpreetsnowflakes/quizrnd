b0c1d8c728719a8c8d73bd3d7395c83e1fdc9179
harpreetsnowflakes <harpreet.singh@snowflakessoftware.com>
2017-02-24 15:59:44 +0530

    Add Files to project

---
+++ application/config/sq_config.php
@@ -0,0 +1,7 @@
+<?php 
+		$sq_base_url='http://[::1]/2017/feb/savsoftquiz/';
+		$sq_hostname='localhost';
+		$sq_dbname='savysoft';
+		$sq_dbusername='root';
+		$sq_dbpassword='';
+		?>
---
+++ application/views/view_result.php
@@ -0,0 +1,6 @@
+<?php
+echo "Hi";
+echo "<pre>";
+//print_r($data);
+echo "</pre>";
+?>
